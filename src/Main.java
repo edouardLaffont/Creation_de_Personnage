a9c86912544b0647b113a933c1fcafd99754007f
edouardLaffont <edouard.laffont@live.com>
2019-02-23 13:24:07 +0100

    Création de la classe NouveauJeu et de fonctionalités inhérentes à cette dernière : - Afficher un choix de personnage - Permettre au joueur de choisir entre ces personnages - Retourner une feuille de personnage prenant en compte les spécificité de chaque classe
    Insertion de ces fonctionalités dans la classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
-
-
 public class Main {
 
 
     public static void main(String[] args) {
-        Personnage warrior = new Warrior("Fury", "Orc","Plaques",1,10,10,10,10,4,"Loup","Hache");
-        Personnage mage = new Mage("Lethan","Elfe","Tissu",1,10,10,10,10,100,"baguette de Sorcier","Elementaire");
         newGame();
-        warrior.startGame();
-        mage.startGame();
-
+        NouveauJeu nouveauJeu = new NouveauJeu();
+        nouveauJeu.newAdventure();
     }
+
     public static void newGame() {
         System.out.println("Vous commencez une nouvelle partie");
     }
+
+
 
 }
--- src/NouveauJeu.java
+++ src/NouveauJeu.java
@@ -1,4 +1,37 @@
+import java.util.Scanner;
+
 public class NouveauJeu {
 
+    public void newAdventure() {
+        this.selectCharacter();
+        Scanner sc = new Scanner(System.in);
+        int nb = sc.nextInt();
+        this.choiceCharacter(nb);
+    }
 
+    public void selectCharacter() {
+        System.out.println("Choix personnage");
+        System.out.println("1 - Guerrier");
+        System.out.println("2 - Mage");
+        System.out.println("3 - Voleur");
+        System.out.println("Que souhaitez-vous choisir ?");
+    }
+
+    public void choiceCharacter(int character) {
+        switch (character) {
+            case 1:
+                System.out.println("Vous avez choisi : Guerrier");
+                Personnage warrior = new Warrior("Fury", "Orc","Plaques",1,10,10,10,10,4,"Loup","Hache");
+                warrior.startGame();
+                break;
+            case 2:
+                System.out.println("Vous avez choisi : Mage");
+                Personnage mage = new Mage("Lethan","Elfe","Tissu",1,10,10,10,10,100,"baguette de Sorcier","Elementaire");
+                mage.startGame();
+                break;
+            case 3:
+                System.out.println("Vous avez choisi : Voleur");
+                break;
+        }
+    }
 }
